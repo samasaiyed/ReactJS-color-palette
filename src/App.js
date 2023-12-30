66bece11f487c01290dd3af058273f76b2dde085
sama <samasaiyed7305@gmail.com>
2023-12-30 19:35:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import tinycolor from 'tinycolor2';
+import React, {useState} from 'react';
 
 function App() {
+  const [baseColor, setBaseColor] = useState('#3498db');
+  const [colorPalette, setColorPalette] = useState([]);
+
+  const GeneratePalette = () => {
+    const palette = [];
+    const base = tinycolor(baseColor);
+
+    for(let i = 0 ; i < 5 ; i++){
+      palette.push({
+      color: base.clone().brighten(i * 10).toString()
+    });
+    
+    };
+
+    setColorPalette(palette);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Color Palette Generator </h1>
+      <div className='container'>
+          <input 
+            type='color' 
+            value={baseColor}
+            onChange={(e) => setBaseColor(e.target.value)}
+          />
+
+          <div className='btn'>
+            <button onClick={GeneratePalette}> Generate palette </button>
+          </div>
+
+          <div className='palette'>
+            {colorPalette.map((color, index) => (
+              <>
+              <div 
+                key={index}
+                className={`color-box 
+                  ${
+                    tinycolor(color.color).isDark() ? 
+                      'light-text' 
+                    : 
+                      'dark-text'}`}
+                style={{backgroundColor: color.color}}  
+              > 
+                {color.color}
+              </div>    
+              </>
+            ))}
+          
+          </div>
+      </div>
+      
     </div>
-  );
+  )
 }
 
 export default App;
